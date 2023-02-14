d9b1aa4af617c835c14516374a75362975c77b87
DevidM9779 <ddavidmilan@hotmail.es>
2023-02-13 23:10:36 -0600

    Added new route for individual track

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -14,6 +14,14 @@ class TrackAPI extends RESTDataSource {
   getAuthor(authorId) {
     return this.get(`author/${authorId}`);
   }
+
+  getTrack(trackId) {
+    return this.get(`track/${trackid}`);
+  }
+
+  getTrackModules(trackId) {
+    return this.get(`track/${trackid}/modules`);
+  }
 }
 
 module.exports = TrackAPI;
